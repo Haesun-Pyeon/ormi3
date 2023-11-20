44051cb5574ec4c56800875a234c3550c5e4e540
Haesun-Pyeon <sme1357@naver.com>
2023-11-21 08:59:35 +0900

    231120 수업까지

---
+++ JWT/accounts/urls.py
@@ -0,0 +1,9 @@
+# accounts/urls.py
+from django.urls import path, include
+from .views import example_view
+
+urlpatterns = [
+    path("", include("dj_rest_auth.urls")),
+    path('join/', include("dj_rest_auth.registration.urls")),
+    path('test/', example_view),
+]
